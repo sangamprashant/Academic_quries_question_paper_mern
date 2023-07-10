9c38a95f3864ce35867b64b95613c1f37e0d90c0
Prashant Srivastav <srivastavp891@gmail.com>
2023-07-10 08:24:22 +0530

    vercel check

--- frontend/src/Components/ReuseComponent/Hero.js
+++ frontend/src/Components/ReuseComponent/Hero.js
@@ -14,9 +14,6 @@ function Hero() {
           <h2>
             We are team of talented designers making websites with Bootstrap
           </h2>
-          <a href="#about" class="btn-get-started scrollto">
-            Get Started
-          </a>
         </div>
       </section>
     </div>
