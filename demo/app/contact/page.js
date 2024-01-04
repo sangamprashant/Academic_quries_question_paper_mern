034dfdabedbf451498a3a2f4ab8af5281e1079f6
Prashant Srivastav <srivastavp891@gmail.com>
2024-01-04 20:09:30 +0530

    net js build

--- demo/app/contact/page.js
+++
@@ -1,9 +0,0 @@
-import { Contact } from '@/component'
-import React from 'react'
-
-function page() {
-  return (<Contact/>
-  )
-}
-
-export default page
--- demo/component/paper/gettingPaper.js
+++
@@ -1,6 +0,0 @@
-"use server";
-export const fetchCourse = async () => {
-  const res = await fetch(`${process.env.DOMAIN}/api/get/course`);
-  const data = await res.json();
-  return data;
-};
